c3dee1c656a0db70e9740572bd3ead15284b6c13
OnurNovruzlu <118649957+OnurNovruzlu@users.noreply.github.com>
2023-05-28 22:26:50 +0400

    Update SecurityController.java

--- src/main/java/az/bina/main/verify/security/SecurityController.java
+++ src/main/java/az/bina/main/verify/security/SecurityController.java
@@ -1,6 +1,7 @@
 package az.bina.main.verify.security;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,23 @@ public class SecurityController {
 
     @GetMapping("/login")
     public String login(){
+        return "login";
+    }
+
+    @GetMapping("/verified")
+    public String verified(Model model){
+        model.addAttribute("success",true);
+        return "login";
+    }
+
+    @GetMapping("/myLogout")
+    public String logout(Model model){
+        model.addAttribute("logout",true);
+        return "login";
+    }
+    @GetMapping("/myLogin")
+    public String failLogin(Model model){
+        model.addAttribute("error",true);
         return "login";
     }
 
